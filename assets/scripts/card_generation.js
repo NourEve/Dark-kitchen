a5638a504d42645db8fe43ea8d5e6401f3f53ba0
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-12-06 14:45:36 +0100

    all

--- assets/scripts/card_generation.js
+++ assets/scripts/card_generation.js
@@ -55,7 +55,7 @@ function generateCards() {
     for (let dish of italianDishes) {
         // Create the article
         let article = document.createElement("article");
-        article.className = dish.dishName;
+        article.className = dish.mainCat + " " + dish.subCat;
 
         // Create the title (dish name)
         let title = document.createElement("h2");
