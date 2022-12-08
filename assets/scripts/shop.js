f4316eeb2d13a46cd77052bde58ce80df9de2371
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2022-12-08 10:47:39 +0100

    shop

--- assets/scripts/shop.js
+++ assets/scripts/shop.js
@@ -1,10 +1,52 @@
-var removeItemButtons = document.getElementsByClassName('removeItem');
-console.log(removeItemButtons);
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready();
+}
 
-for (var i = 0; i < removeItemButtons.length; i++) {
-    var button = removeItemButtons[i];
-    button.addEventListener('click', function(event) {
-        var buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
-    })
+function ready() {
+    var removeItemButtons = document.getElementsByClassName('removeItem');
+
+    for (var i = 0; i < removeItemButtons.length; i++) {
+        var button = removeItemButtons[i];
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove();
+    updateTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target; 
+
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updateTotal()
+}
+
+function updateTotal() {
+    var cartItemContainer = document.getElementsByClassName("cart-items")[0]
+    var cartRows = cartItemContainer.getElementsByClassName("cart-row")
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('€', ''));
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+
+    document.getElementsByClassName('cart-total-price')[0].innerText = "€" + total;
 }
