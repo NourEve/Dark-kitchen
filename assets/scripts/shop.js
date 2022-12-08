d62f830f5d971dfb436a79e047a006f4e7b0135c
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2022-12-08 15:37:32 +0100

    changed scripts

--- assets/scripts/shop.js
+++ assets/scripts/shop.js
@@ -1,3 +1,21 @@
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div');
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartRowContents =
+        `   <div class="cart-item cart-column">
+        <img class="cart-item-image" src="${imageSrc}">
+        <span class="cart-item-title">${title}</span>
+    <span class="cart-price cart-column">${price}</span>
+    <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" value="1" min="1" />
+        <button class="removeItem" type="button">REMOVE</button>
+    </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    console.log(updateTotal())
+}
+
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready)
 } else {
@@ -6,31 +24,32 @@ if (document.readyState == 'loading') {
 }
 
 function ready() {
-    var removeItemButtons = document.getElementsByClassName('removeItem');
-
-    for (var i = 0; i < removeItemButtons.length; i++) {
-        var button = removeItemButtons[i];
-        button.addEventListener('click', removeCartItem)
-    }
-
-    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-
-    for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
-    }
-
     var addToCart = document.getElementsByClassName("article__addButton")
     for (var i = 0; i < addToCart.length; i++) {
         var plus = addToCart[i];
         plus.addEventListener('click', addToCartClicked)
     }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var removeItemButtons = document.getElementsByClassName('removeItem');
+    for (var i = 0; i < removeItemButtons.length; i++) {
+        var button = removeItemButtons[i];
+        button.addEventListener('click', removeCartItem)
+    }
 }
+
+
 
 function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove();
     updateTotal()
+    console.log('.')
 }
 
 function quantityChanged(event) {
@@ -48,29 +67,12 @@ function addToCartClicked(event) {
     var title = shopItem.getElementsByClassName("article__name")[0].innerText;
     var price = shopItem.getElementsByClassName("article__container__price")[0].innerText;
     var imageSrc = shopItem.getElementsByClassName("article__img")[0].src;
-    console.log(title, price, imageSrc)
     addItemToCart(title, price, imageSrc)
+    updateTotal()
 }
 
-function addItemToCart(title, price, imageSrc) {
-    var cartRow = document.createElement('div');
-    cartRow.classList.add('cart-row')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartRowContents = `<div class="cart-row">
-    <div class="cart-item cart-column">
-    <img class="cart-item-image" src="${imageSrc}"
-    <span class="cart-item-title" style="color: white" ;>${title}</span>
-  </div>
-  <span class="cart-price cart-column">${price}</span>
-  <div class="cart-quantity cart-column">
-    <input class="cart-quantity-input" type="number" value="1" />
-    <button class="removeItem" type="button">REMOVE</button>
-  </div>
-  </div>`
-  cartRow.innerHTML = cartRowContents
-    cartItems.append(cartRow)
 
-}
+
 
 function updateTotal() {
     var cartItemContainer = document.getElementsByClassName("cart-items")[0]
@@ -83,7 +85,11 @@ function updateTotal() {
         var price = parseFloat(priceElement.innerText.replace('€', ''));
         var quantity = quantityElement.value
         total = total + (price * quantity)
+
     }
     total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = "€" + total;
+    document.getElementsByClassName('cart-total-price')[0].innerText = "€" + total.toFixed(2);
+
+    return total;
+
 }
