578d718ed5180b00fe4153a341081536ad2a402c
Nour Everaert <117478874+NourEve@users.noreply.github.com>
2022-12-14 19:39:34 +0100

    Delete assets directory

--- assets/scripts/dark_mode.js
+++
@@ -1,9 +0,0 @@
-function darkMode() {
-  document.body.classList.toggle("dark");
-}
-
-const btnSwitch = document.getElementsByClassName("header__colormode");
-console.log(btnSwitch); // maybe change the class name
-for (const btn of btnSwitch) {
-  btn.addEventListener("click", darkMode);
-}
